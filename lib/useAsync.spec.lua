94b3433b6cf3fff240b09bd7b07fd9d35432da4c
Lucas Gangstad <lucas@lasttalon.dev>
2023-03-11 19:30:50 -0600

    Add useAsync

---
+++ lib/useAsync.spec.lua
@@ -0,0 +1,11 @@
+local useAsync = require(script.Parent.useAsync)
+
+return function()
+	FIXME()
+
+	describe("Some Test", function()
+		it("should be true", function()
+			expect(true).to.equal(true)
+		end)
+	end)
+end

7beae7ddc5179b5106541ba5aabab299fa537029
Lucas Gangstad <lucas@lasttalon.dev>
2023-03-12 00:25:18 -0600

    Fix library interface

--- lib/init.lua
+++ lib/init.lua
@@ -1,7 +1,11 @@
+local useChange = require(script.useChange)
 local useContextAction = require(script.useContextAction)
 local useMemo = require(script.useMemo)
+local useReducer = require(script.useReducer)
 
 return {
+	useChange = useChange,
 	useContextAction = useContextAction,
 	useMemo = useMemo,
+	useReducer = useReducer,
 }

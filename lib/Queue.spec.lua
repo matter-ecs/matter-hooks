cfd469c87e08ffb379e8edf338e13c2041114c11
Lucas Gangstad <lucas@lasttalon.dev>
2023-03-12 00:04:46 -0600

    Add empty tests

---
+++ lib/Queue.spec.lua
@@ -0,0 +1,11 @@
+local Queue = require(script.Parent.Queue)
+
+return function()
+	FIXME()
+
+	describe("Some Test", function()
+		it("should be true", function()
+			expect(true).to.equal(true)
+		end)
+	end)
+end
---
+++ lib/useStream.spec.lua
@@ -0,0 +1,11 @@
+local useStream = require(script.Parent.useStream)
+
+return function()
+	FIXME()
+
+	describe("Some Test", function()
+		it("should be true", function()
+			expect(true).to.equal(true)
+		end)
+	end)
+end

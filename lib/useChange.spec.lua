127a824774d62da92288ac706a940ad236563873
Lucas Gangstad <lucas@lasttalon.dev>
2023-03-11 19:31:20 -0600

    Add useChange

---
+++ lib/useChange.spec.lua
@@ -0,0 +1,11 @@
+local useChange = require(script.Parent.useChange)
+
+return function()
+	FIXME()
+
+	describe("Some Test", function()
+		it("should be true", function()
+			expect(true).to.equal(true)
+		end)
+	end)
+end

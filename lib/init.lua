7e805de6c6657b90ecc84e7c30e635a7d6303536
Lucas Gangstad <lucas@lasttalon.dev>
2023-03-15 18:12:21 -0500

    Update library interface
    This also includes the missed change for useAsync.

--- lib/init.lua
+++ lib/init.lua
@@ -1,11 +1,15 @@
+local useAsync = require(script.useAsync)
 local useChange = require(script.useChange)
 local useContextAction = require(script.useContextAction)
 local useMemo = require(script.useMemo)
 local useReducer = require(script.useReducer)
+local useStream = require(script.useStream)
 
 return {
+	useAsync = useAsync,
 	useChange = useChange,
 	useContextAction = useContextAction,
 	useMemo = useMemo,
 	useReducer = useReducer,
+	useStream = useStream,
 }

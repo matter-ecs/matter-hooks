b901db94b4646e4f78f4b47d3dd86a9ac4deb540
Lucas Gangstad <lucas@lasttalon.dev>
2023-03-18 22:54:37 -0500

    Add to library interface

--- lib/init.lua
+++ lib/init.lua
@@ -1,6 +1,7 @@
 local useAsync = require(script.useAsync)
 local useChange = require(script.useChange)
 local useContextAction = require(script.useContextAction)
+local useMap = require(script.useMap)
 local useMemo = require(script.useMemo)
 local useReducer = require(script.useReducer)
 local useStream = require(script.useStream)
@@ -13,6 +14,7 @@ return {
 	useAsync = useAsync,
 	useChange = useChange,
 	useContextAction = useContextAction,
+	useMap = useMap,
 	useMemo = useMemo,
 	useReducer = useReducer,
 	useStream = useStream,
